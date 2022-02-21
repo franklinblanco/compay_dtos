cc6985fc66a41f0e47dbfc69bed4a51ea469b18b
franklin <franklinblanco62@gmail.com>
2022-02-21 14:50:53 -0400

    first commit

---
+++ src/test/java/com/blancoinfante/compay_dtos/AppTest.java
@@ -0,0 +1,20 @@
+package com.blancoinfante.compay_dtos;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
