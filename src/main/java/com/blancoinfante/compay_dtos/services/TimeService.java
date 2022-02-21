48b4435ed549d7e2425bd02354fa50bf3c78f2a7
franklin <franklinblanco62@gmail.com>
2022-02-21 19:25:39 -0400

    Made timeservice an object.

--- src/main/java/com/blancoinfante/compay_dtos/services/TimeService.java
+++ src/main/java/com/blancoinfante/compay_dtos/services/TimeService.java
@@ -1,12 +1,8 @@
 package com.blancoinfante.compay_dtos.services;
 
-import org.springframework.stereotype.Service;
-
 import java.util.Date;
-
-@Service
 public class TimeService {
-    //TODO: Handle all time related methods
+    //TODO: Make this a service. Not an object to be created.
     public long getTimeInMillis(){
         Date date = new Date();
         return date.getTime();
